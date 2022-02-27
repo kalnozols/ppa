3f1a7713fb61bd30bff3fd5fb778fb9a2fee8dd8
Kristaps Kalnozols <kristaps.kalnozols@gmail.com>
2022-02-27 17:23:34 +0200

    Update jauns.js

--- jauns.js
+++ jauns.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-	$('#btnLogin').click(function(){
+	$('#btnLogin').click(function(event){
 	event.preventDefault();
 	IdValue = $('input[name=id-name]').val(); 
 	window.location.href = "http://ppa.lv/id/"+ IdValue+".pdf"; 
